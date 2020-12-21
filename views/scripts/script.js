d5ccf4ef64625d61fa272f0276af767daa44ab6d
Kashix <36087508+otkashix@users.noreply.github.com>
2020-12-21 05:34:13 +0100

    Added dark/light mode

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var mode = "";
+    var mode = "dark";
     var time = 0;
     var words = 0;
     var speed = 0;
@@ -10,6 +10,32 @@ $(document).ready(function() {
 
     document.getElementById("message").innerHTML = msg;
     document.getElementById("lang").innerHTML = lang;
+
+    document.getElementById("mode").addEventListener("click",
+        function changeMode(){
+            if(mode === "dark"){
+                mode = "light";
+                document.body.style.backgroundColor = "#ffffff";
+                document.getElementById("mode").textContent = "Dark";
+                document.body.style.color = "black";
+                document.getElementById("mode").style.backgroundColor = "#011d20";
+                document.getElementById("mode").style.color = "white";
+                document.getElementById("write").style.backgroundColor = "#ffffff";
+                document.getElementById("message").style.backgroundColor = "#ffffff"; 
+            }else if(mode === "light"){
+                mode = "dark";
+                document.body.style.backgroundColor = "#011d20";
+                document.getElementById("mode").textContent = "Light";
+                document.body.style.color = "white";
+                document.getElementById("mode").style.backgroundColor = "#ffffff";
+                document.getElementById("mode").style.color = "black";
+                document.getElementById("write").style.backgroundColor = "#011d20";
+                document.getElementById("message").style.backgroundColor = "#011d20";
+            }
+        }
+    );
+
+    
 
     setInterval(function() {
         document.getElementById("time").innerHTML = time;
